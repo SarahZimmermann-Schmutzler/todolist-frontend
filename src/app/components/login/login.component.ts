8b54e88645505cbe4c1c14bce021bb0db8f32c05
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-14 12:02:00 +0100

    login()

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,14 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  email = '';
+  username = '';
   password = '';
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
-  login() {
-    // Logik, um mit Backend zu kommunizieren
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    try{
+      let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions);
+      let json = await resp.json();
+      localStorage.setItem('Token', json.token)
+    } catch(e) {
+      console.error(e);
+    }
   }
 }
